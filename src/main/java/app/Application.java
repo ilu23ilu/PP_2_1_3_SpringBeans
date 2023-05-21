cbcad19590a7fc85ea550e928f81bc9ebf656b6a
Chudov Ilya <chudow-iluha@mail.ru>
2023-05-21 16:40:26 +0300

    first commit

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -15,6 +15,6 @@ public class Application {
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
+        ((AnnotationConfigApplicationContext)applicationContext).close();
     }
-
 }
--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,28 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(getTimer().getTime());
         System.out.println("________________________");
+    }
+
+    @Bean
+    public Timer getTimer() {
+        Timer timer = new Timer();
+        return timer;
     }
 }
